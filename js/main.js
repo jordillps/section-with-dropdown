7b46d521ad0460955e26168b4cf8fd3fae436169
jordillps <jordillps@gmail.com>
2022-10-07 12:41:48 +0200

    Improving the design

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 
+let mobile_NavBar = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+    if(navBarToggle.classList.contains('open')){
+        navBarToggle.classList.remove('open');
+        mobile_NavBar.classList.add('hidden__menu');
+    }else{
+        navBarToggle.classList.add('open');
+        mobile_NavBar.classList.remove('hidden__menu');
+    }
+});
+
+
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 let arg;
 function showSubmenu(arg) {
+  let submenu = document.getElementById(arg);
   document.getElementById(arg).classList.toggle("show");
   document.querySelector('.' + arg).classList.toggle("show");
+  
 }
 
 // Close the dropdown if the user clicks outside of it
